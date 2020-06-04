c3484b8acf3b78d557ce7bc5913c28bd0cd632f3
Arashi Takano <darksidemoon593@icloud.com>
2020-06-04 16:04:17 +0900

    notification with pending single top activity

--- app/src/main/java/com/arstkn/lock_screen_activity/KeepScreenActivity.kt
+++ app/src/main/java/com/arstkn/lock_screen_activity/KeepScreenActivity.kt
@@ -1,18 +1,23 @@
 package com.arstkn.lock_screen_activity
 
+import android.app.NotificationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_keep_screen.*
 
 class KeepScreenActivity : AppCompatActivity() {
+    private var notificationManager: NotificationManagerCompat? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_keep_screen)
+        notificationManager = NotificationManagerCompat.from(this)
 
         stop_activity.setOnClickListener {
             finish()
+            notificationManager!!.cancel(1)
         }
 
         window.addFlags(
