5cfc85c197c31f92a3dcae4c46d9a3dd80b72317
Arashi Takano <darksidemoon593@icloud.com>
2020-06-08 17:46:43 +0900

    stop after 30 seconds

--- app/src/main/java/com/arstkn/lock_screen_activity/KeepScreenActivity.kt
+++ app/src/main/java/com/arstkn/lock_screen_activity/KeepScreenActivity.kt
@@ -3,13 +3,16 @@ package com.arstkn.lock_screen_activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.WindowManager
 import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_keep_screen.*
 
 class KeepScreenActivity : AppCompatActivity() {
+    private val handler = Handler()
     private var notificationManager: NotificationManagerCompat? = null
+    private var service: Intent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,18 +22,29 @@ class KeepScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_keep_screen)
         notificationManager = NotificationManagerCompat.from(this)
 
-        val service = Intent(this, NotificationService::class.java)
+        service = Intent(this, NotificationService::class.java)
         startForegroundService(service)
 
         stop_activity.setOnClickListener {
-            stopService(service)
-
+            handler.removeCallbacks(runnable)
             finishAndRemoveTask()
         }
+
+        handler.postDelayed(runnable, 30000)
 
         window.addFlags(
             WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                     or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
                     or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(service)
+    }
+
+    private val runnable = Runnable {
+        Log.d("runnable", "run")
+        finishAndRemoveTask()
     }
 }
